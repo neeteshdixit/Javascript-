b3b8b12170249cefd2ff95096d16ea0067d59469
Neetesh Dixit <neeteshdixit@gmail.com>
2026-01-22 10:29:36 +0530

    add initial HTML and JavaScript files for event listener functionality

---
+++ full-java-script-with events listener/index.js
@@ -0,0 +1,31 @@
+
+// function changeText(event) {
+//     console.log(event);
+//     let fpara = document.getElementById('fpara');
+//     fpara.textContent = 'Hello Ayushi! This is changed using JavaScript.';
+
+// }
+
+// let fpara = document.getElementById('fpara');
+// fpara.addEventListener('click', changeText);
+
+// let link = document.getElementById('link');
+
+
+// link.addEventListener('click', function(event) {
+//     event.preventDefault(); // ye kisi or page pr jane se rokta hai
+//     link.textContent = 'Link Clicked!';
+//     console.log('The link was clicked, but default action is prevented.');
+// });// function to prevent default action of anchor tag 
+
+
+// let paras = document.querySelectorAll('p');
+
+// function alertPara(event){
+//      alert("you clicked paragraph " + event.target.textContent);
+// }
+// for(var i = 0; i<paras.length; i++) {
+//     var para = paras[i];
+//     para.addEventListener('click', alertPara);
+//     para.textContent = 'Paragraph ' + (i + 1) + ' clicked!';
+// }    
