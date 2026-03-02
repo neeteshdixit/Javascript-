90d9d41e25bdcd181d9c8dba05967234ce0f1e92
neeteshdixit <dixitneetesh857@gmail.com>
2026-03-02 15:22:53 +0530

    routing concept

--- backend-development/index.js
+++ backend-development/index.js
@@ -2,33 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res)=> {
-    console.log("Hello World");
-    res.send("Hello World");
-})
+// import router
+const router = require("./routes/router.js");
 
-//requests 
-
-//get request
-app.get("/get", (req, res)=> {
-    console.log("Get Request");
-    res.send("Get Request");
-})
-// post request
-app.post("/post",(req, res)=>{
-    console.log("post request")
-    res.send("post request")
-})
-// put request
-app.put("/put", (req,res)=>{
-    console.log("put request")
-    res.send("put request")
-})
-// delete request
-app.delete("/delete", (req,res)=>{
-    console.log("delete request")
-    res.send("delete request")
-})
+app.use("/routing", router);
 
 app.listen(port, ()=> {
     console.log(`Server is running on port http://localhost:${port}`);
---
+++ backend-development/routes/router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/get", (req, res)=> {
+    res.send("Get Request");
+})
+// send file
+router.get("/", (req, res)=> {
+    console.log("Hello World");
+    res.sendFile("./dummy.html", {root: __dirname});
+})
+
+//requests 
+
+//get request
+router.get("/get", (req, res)=> {
+    console.log("Get Request");
+    res.send("Get Request");
+})
+// post request
+router.post("/post",(req, res)=>{
+    console.log("post request")
+    res.send("post request")
+})
+// put request
+router.put("/put", (req,res)=>{
+    console.log("put request")
+    res.send("put request")
+})
+// delete request
+router.delete("/delete", (req,res)=>{
+    console.log("delete request")
+    res.send("delete request")
+})
+
