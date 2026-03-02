9dfb9cd9aee448ed46863f8bdc517f15aefbf421
neeteshdixit <dixitneetesh857@gmail.com>
2026-03-02 15:06:18 +0530

    feat: add to-regex-range package for regex range matching
    feat: add LICENSE and README for touch package
    
    feat: implement nodetouch CLI for touch functionality
    
    feat: create index.js for touch package functionality
    
    feat: add package.json for touch package
    
    ci: add GitHub Actions workflow for undefsafe releases
    
    style: add .jscsrc and .jshintrc for undefsafe code style
    
    ci: add .travis.yml for undefsafe CI configuration
    
    feat: add LICENSE and README for undefsafe package
    
    feat: implement undefsafe function for safe property access
    
    feat: add example for undefsafe usage
    
    feat: add package.json for undefsafe package

--- backend-development/index.js
+++ backend-development/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const app = express();
-const port = 6000;
+const port = 3000;
 
 app.get("/", (req, res)=> {
+    console.log("Hello World");
     res.send("Hello World");
+})
+
+//requests 
+
+//get request
+app.get("/get", (req, res)=> {
+    console.log("Get Request");
+    res.send("Get Request");
+})
+// post request
+app.post("/post",(req, res)=>{
+    console.log("post request")
+    res.send("post request")
+})
+// put request
+app.put("/put", (req,res)=>{
+    console.log("put request")
+    res.send("put request")
+})
+// delete request
+app.delete("/delete", (req,res)=>{
+    console.log("delete request")
+    res.send("delete request")
 })
 
 app.listen(port, ()=> {
